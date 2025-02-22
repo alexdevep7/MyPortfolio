2e5e5ed758b3e0472f08b7613c8e751e7c2b7e0f
alexdevep7 <100159655+alexdevep7@users.noreply.github.com>
2025-02-22 19:50:22 -0300

    new changes again

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*==================== typed js ====================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Product Manager"],
+  strings: ["Android Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 100,
